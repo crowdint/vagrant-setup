ee0ec44a72a13b81b50edfd613c73eea5007a59d
Mario Zaizar <mariozaizar@gmail.com>
2011-08-30 13:45:59 -0500

    Split the general recipe, into system & system::utils

--- cookbooks/general/recipes/default.rb
+++ cookbooks/general/recipes/default.rb
@@ -7,9 +7,8 @@ script "Update ubuntu packages" do
   interpreter "bash"
   user "root"
   code <<-EOH
-    if [ ! -e /etc/profile.d/vibash.sh ]; then
-      apt-get update
-    fi
+    apt-get update
+    updatedb
   EOH
 end
 
@@ -30,15 +29,4 @@ script "Fixing the 'Please check that your locale settings' error" do
       dpkg-reconfigure locales
     fi
   EOH
-end
-
-package "htop" do
-  action :install
-end
-
-file "/etc/profile.d/vibash.sh" do
-  action :create
-  owner "root"
-  group "root"
-  content "set -o vi"
 end
